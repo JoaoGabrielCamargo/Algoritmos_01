55625d78a0c5457ad4e529ac339e48cac352d1f2
Gabriel Viana <joao.camargo3@estudante.ifms.edu.br>
2025-05-31 16:56:40 -0400

    Update atividade_02.c

--- atividade_02.c
+++ atividade_02.c
@@ -20,8 +20,8 @@ int main(){
       scanf("%d", &numeros[contador_01][contador_02]);
      }
     }
-    for(contador_01 = 0; contador_01 < 3; contador_01++){
-     for(contador_02 = 0; contador_02 < 3; contador_02++){
+    for(contador_01 = 0; contador_01 < verificar_X; contador_01++){
+     for(contador_02 = 0; contador_02 < verificar_Y; contador_02++){
       soma += numeros[contador_01][contador_02];
      }
     media = ((float)(soma / (verificar_X * verificar_Y)));

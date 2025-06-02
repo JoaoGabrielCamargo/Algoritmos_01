72dc6358e2cbdabb666f1583e97282f042bb6e34
Gabriel Viana <joao.camargo3@estudante.ifms.edu.br>
2025-06-02 11:31:36 -0400

    Update atividade_06.c

--- atividade_06.c
+++ atividade_06.c
@@ -5,7 +5,8 @@ int main(){
   int matriz_a[100][100];
   int matriz_b[100][100];
   int matriz_c[100][100];
-  int contador, contador_2;
+  int matriz_temp[100][100];
+  int contador, contador_2, contador_3;
   
   printf("Tamanho das matrizes: ");
   scanf("%d", &tamanho);
@@ -13,8 +14,23 @@ int main(){
   for(contador = 0; contador < tamanho; contador++){
     for(contador_2 = 0; contador_2 < tamanho; contador_2++){
       scanf("%d", &matriz_a[contador][contador_2]);
+    }
+  }
+  for(contador = 0; contador < tamanho; contador++){
+    for(contador_2 = 0; contador_2 < tamanho; contador_2++){
       scanf("%d", &matriz_b[contador][contador_2]);
-      matriz_c[contador][contador_2] = matriz_b[contador][contador_2] * matriz_a[contador][contador_2];
+    }
+  }
+  for(contador = 0; contador < tamanho; contador++){
+    for(contador_2 = 0; contador_2 < tamanho; contador_2++){
+      matriz_c[contador][contador_2] = 0;
+    }
+  }
+  for(contador = 0; contador < tamanho; contador++){
+    for(contador_2 = 0; contador_2 < tamanho; contador_2++){
+        for(contador_3 = 0; contador_3 < tamanho; contador_3++){
+          matriz_c[contador][contador_2] += matriz_a[contador][contador_3] * matriz_b[contador_3][contador_2];
+        }  
     }
   }
   printf("\n");
